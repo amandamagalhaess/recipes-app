ed3c8c6e852c4d34fc627fb89d26fd95e31aea9b
Pedro Minutti <pedro.cminutti@gmail.com>
2023-07-10 19:29:34 -0300

    7 - Implemente o header de acordo com a necessidade de cada tela

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
 import RecipesProvider from './context/RecipesProvider';
 import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/meals" component={ Meals } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       </Switch>
     </RecipesProvider>
   );
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import Header from '../components/Header';
+
 function Meals() {
   return (
-    <h1>Meals</h1>
+    <Header />
   );
 }
 
