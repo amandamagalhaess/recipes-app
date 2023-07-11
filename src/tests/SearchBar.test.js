dc50194309fd1974ce1c519ddeaa67f2e8acdc93
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-07-11 17:13:54 -0300

    Requisito 15 - cobertura de testes

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -3,42 +3,47 @@ import { screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import { renderWithRouter } from './helpers/renderWithRouter';
 
+const searchID = 'search-top-btn';
+const searchInputID = 'search-input';
+const searchRadioName = 'name-search-radio';
+const searchButtonID = 'exec-search-btn';
+
 describe('Testa o componente Search Bar', () => {
   it('Testa se é possivel fazer a busca na página de Meals', async () => {
     renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
-    const searchTopBtn = screen.getByTestId('search-top-btn');
+    const searchTopBtn = screen.getByTestId(searchID);
     userEvent.click(searchTopBtn);
 
-    const textInput = screen.getByTestId('search-input');
-    userEvent.type(textInput, 'Arrabiata');
+    const textInput = screen.getByTestId(searchInputID);
+    userEvent.type(textInput, 'a');
 
-    const radioName = screen.getByTestId('name-search-radio');
-    userEvent.click(radioName);
+    const radioFirstLetter = screen.getByTestId('first-letter-search-radio');
+    userEvent.click(radioFirstLetter);
 
-    const btnSearch = screen.getByTestId('exec-search-btn');
+    const btnSearch = screen.getByTestId(searchButtonID);
     userEvent.click(btnSearch);
 
     await waitFor(() => expect(screen.getByTestId('0-recipe-card')).toBeInTheDocument());
 
     const recipeName = screen.getByTestId('0-card-name');
     expect(recipeName).toBeInTheDocument();
-    expect(recipeName.innerHTML).toBe('Spicy Arrabiata Penne');
+    expect(recipeName.innerHTML).toBe('Apple Frangipan Tart');
   });
 
   it('Testa se é possivel fazer a busca na página de Drinks', async () => {
     renderWithRouter(<App />, { initialEntries: ['/drinks'] });
 
-    const searchTopBtn = screen.getByTestId('search-top-btn');
+    const searchTopBtn = screen.getByTestId(searchID);
     userEvent.click(searchTopBtn);
 
-    const textInput = screen.getByTestId('search-input');
+    const textInput = screen.getByTestId(searchInputID);
     userEvent.type(textInput, 'lemon');
 
     const radioIngredient = screen.getByTestId('ingredient-search-radio');
     userEvent.click(radioIngredient);
 
-    const btnSearch = screen.getByTestId('exec-search-btn');
+    const btnSearch = screen.getByTestId(searchButtonID);
     userEvent.click(btnSearch);
 
     await waitFor(() => expect(screen.getByTestId('0-recipe-card')).toBeInTheDocument());
@@ -46,5 +51,91 @@ describe('Testa o componente Search Bar', () => {
     const recipeName = screen.getByTestId('0-card-name');
     expect(recipeName).toBeInTheDocument();
     expect(recipeName.innerHTML).toBe('A True Amaretto Sour');
+  });
+
+  it('Testa se quando a API retornar apenas uma receita na página de Meals o usuário é redirecionado para a tela de detalhes', async () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/meals'] });
+
+    const searchTopBtn = screen.getByTestId(searchID);
+    userEvent.click(searchTopBtn);
+
+    const textInput = screen.getByTestId(searchInputID);
+    userEvent.type(textInput, 'Apple Frangipan Tart');
+
+    const radioName = screen.getByTestId(searchRadioName);
+    userEvent.click(radioName);
+
+    const btnSearch = screen.getByTestId(searchButtonID);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      const { pathname } = history.location;
+      expect(pathname).toBe('/meals/52768');
+    });
+  });
+
+  it('Testa se quando a API retornar apenas uma receita na página de Drinks o usuário é redirecionado para a tela de detalhes', async () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+
+    const searchTopBtn = screen.getByTestId(searchID);
+    userEvent.click(searchTopBtn);
+
+    const textInput = screen.getByTestId(searchInputID);
+    userEvent.type(textInput, 'A True Amaretto Sour');
+
+    const radioName = screen.getByTestId(searchRadioName);
+    userEvent.click(radioName);
+
+    const btnSearch = screen.getByTestId(searchButtonID);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      const { pathname } = history.location;
+      expect(pathname).toBe('/drinks/17005');
+    });
+  });
+
+  it('Testa se quando não retornar nenhuma receita na página Meals o usuário é alertado', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+
+    const searchTopBtn = screen.getByTestId(searchID);
+    userEvent.click(searchTopBtn);
+
+    const textInput = screen.getByTestId(searchInputID);
+    userEvent.type(textInput, 'blablabla');
+
+    const radioName = screen.getByTestId(searchRadioName);
+    userEvent.click(radioName);
+
+    const btnSearch = screen.getByTestId(searchButtonID);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      const alertSpy = jest.spyOn(global, 'alert');
+      expect(alertSpy).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.');
+      alertSpy.mockRestore();
+    });
+  });
+
+  it('Testa se quando não retornar nenhuma receita na página Drinks o usuário é alertado', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+
+    const searchTopBtn = screen.getByTestId(searchID);
+    userEvent.click(searchTopBtn);
+
+    const textInput = screen.getByTestId(searchInputID);
+    userEvent.type(textInput, 'blablabla');
+
+    const radioName = screen.getByTestId(searchRadioName);
+    userEvent.click(radioName);
+
+    const btnSearch = screen.getByTestId(searchButtonID);
+    userEvent.click(btnSearch);
+
+    await waitFor(() => {
+      const alertSpy = jest.spyOn(global, 'alert');
+      expect(alertSpy).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.');
+      alertSpy.mockRestore();
+    });
   });
 });
