28f0bc2bd67b95df9d4ca97779bc53a868be960f
Aline Machado <alinedasmachado@gmail.com>
2023-07-12 12:11:08 -0300

    28 - Desenvolva um botão de nome "Start Recipe" que deve ficar fixo na parte de baixo da tela o tempo todo

--- src/components/RecomendationCarousel.js
+++ src/components/RecomendationCarousel.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import RecipesContext from '../context/RecipesContext';
 import '../styles/RecomendationCarousel.css';
+import StartRecipe from './StartRecipe';
 
 function RecommendationCarousel() {
   const { meals, drinks } = useContext(RecipesContext);
@@ -58,7 +59,11 @@ function RecommendationCarousel() {
           <button onClick={ handleScrollNext }>Próxima</button>
         </>
       )}
+      <div>
+        <StartRecipe />
+      </div>
     </div>
+
   );
 }
 
---
+++ src/components/StartRecipe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StartRecipe() {
+  return (
+    <button className="btnStart" data-testid="start-recipe-btn">
+      Start Recipe
+    </button>
+  );
+}
+
+export default StartRecipe;
