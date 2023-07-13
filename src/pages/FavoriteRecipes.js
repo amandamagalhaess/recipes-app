f3d6a3534e0c22349d5968cb01f2b6dfddd1af6b
Pedro Minutti <pedro.cminutti@gmail.com>
2023-07-13 17:48:06 -0300

    50 ao 56

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,8 +1,143 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  // _________________________________________________________
+  useEffect(() => {
+    const storedFavoriteRecipes = localStorage.getItem('favoriteRecipes');
+    if (storedFavoriteRecipes) {
+      setFavoriteRecipes(JSON.parse(storedFavoriteRecipes));
+    }
+  }, []);
+
+  const handleShareButton = (value) => {
+    const parsedValue = JSON.parse(value);
+    let url;
+    if (parsedValue.type === 'meal') {
+      url = `http://localhost:3000/meals/${parsedValue.id}`;
+    } else {
+      url = `http://localhost:3000/drinks/${parsedValue.id}`;
+    }
+
+    navigator.clipboard.writeText(url);
+    const divFavorites = document.getElementById('divFavorites');
+    const copyLink = document.createElement('p');
+    copyLink.innerHTML = 'Link copied!';
+    divFavorites.appendChild(copyLink);
+  };
+
+  const handleDesfavoriteButton = (value) => {
+    const storedFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const parsedValue = JSON.parse(value);
+    const newFavoriteRecipes = storedFavoriteRecipes
+      .filter((recipes) => recipes.id !== parsedValue.id);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+    setFavoriteRecipes(newFavoriteRecipes);
+  };
+
+  const handleFilter = ({ target }) => {
+    const storedFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+    if (target.dataset.testid === 'filter-by-all-btn') {
+      setFavoriteRecipes(storedFavoriteRecipes);
+    } else if (target.dataset.testid === 'filter-by-meal-btn') {
+      const doneMeals = storedFavoriteRecipes
+        .filter((recipes) => recipes.type === 'meal');
+      setFavoriteRecipes(doneMeals);
+    } else {
+      const doneDrink = storedFavoriteRecipes
+        .filter((recipes) => recipes.type === 'drink');
+      setFavoriteRecipes(doneDrink);
+    }
+  };
+
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ handleFilter }
+      >
+        All
+      </button>
+
+      <button type="button" data-testid="filter-by-meal-btn" onClick={ handleFilter }>
+        Meals
+      </button>
+
+      <button type="button" data-testid="filter-by-drink-btn" onClick={ handleFilter }>
+        Drinks
+      </button>
+
+      {favoriteRecipes.map((recipegit, index) => (
+        <div id="divFavorites" key={ recipegit.id }>
+          <a href={ `/${recipegit.type}s/${recipegit.id} ` }>
+            <img
+              src={ recipegit.image }
+              alt={ recipegit.name }
+              data-testid={ `${index}-horizontal-image` }
+            />
+
+          </a>
+          {recipegit.type === 'drink' ? (
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {recipegit.alcoholicOrNot}
+            </p>
+          ) : (
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {recipegit.nationality}
+              {' '}
+              -
+              {' '}
+              {recipegit.category}
+            </p>
+          )}
+          <a href={ `/${recipegit.type}s/${recipegit.id} ` }>
+            <p data-testid={ `${index}-horizontal-name` }>
+              {recipegit.name}
+            </p>
+          </a>
+
+          <p data-testid={ `${index}-horizontal-done-date` }>
+            {recipegit.doneDate}
+          </p>
+          {/* {recipegit.tags.slice(0, 2).map((tag, tagIndex) => (
+            <span
+              key={ tagIndex }
+              data-testid={ `${index}-${tag}-horizontal-tag` }
+            >
+              {tag}
+            </span>
+          ))} */}
+          <button
+            type="button"
+            onClick={ () => handleShareButton(JSON.stringify(recipegit)) }
+          >
+            <img
+              src={ shareIcon }
+              alt=""
+              data-testid={ `${index}-horizontal-share-btn` }
+            />
+          </button>
+          <button
+            type="button"
+            onClick={ () => handleDesfavoriteButton(JSON.stringify(recipegit)) }
+          >
+            <img
+              src={ blackHeartIcon }
+              alt=""
+              data-testid={ `${index}-horizontal-favorite-btn` }
+            />
+          </button>
+
+        </div>
+      ))}
+    </div>
   );
 }
---
+++ src/tests/FavoriteRecipes.test.js
@@ -0,0 +1,156 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouter } from './helpers/renderWithRouter';
+
+describe('Testa a página DoneRecipes', () => {
+  localStorage.setItem('favoriteRecipes', JSON.stringify([
+    {
+      id: '52977',
+      type: 'meal',
+      area: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    },
+    {
+      id: '178319',
+      type: 'drink',
+      area: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    },
+  ]));
+  beforeEach(() => {
+    renderWithRouter(<App />, { initialEntries: ['/favorite-recipes'] });
+  });
+  it('Testa se a página contém os elementos esperados', () => {
+    const filterByAllBtn = screen.getByTestId('filter-by-all-btn');
+    const filterByFoodBtn = screen.getByTestId('filter-by-meal-btn');
+    const filterByDrinkBtn = screen.getByTestId('filter-by-drink-btn');
+
+    expect(filterByAllBtn).toBeInTheDocument();
+    expect(filterByFoodBtn).toBeInTheDocument();
+    expect(filterByDrinkBtn).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém as receitas feitas', () => {
+    const image = screen.getAllByTestId(/-horizontal-image/i);
+    const topText = screen.getAllByTestId(/-horizontal-top-text/i);
+    const name = screen.getAllByTestId(/-horizontal-name/i);
+    const doneDate = screen.getAllByTestId(/-horizontal-done-date/i);
+    const shareBtn = screen.getAllByTestId(/-horizontal-share-btn/i);
+
+    expect(image).toHaveLength(2);
+    expect(topText).toHaveLength(2);
+    expect(name).toHaveLength(2);
+    expect(doneDate).toHaveLength(2);
+    expect(shareBtn).toHaveLength(2);
+  });
+
+  it('Testa se ao clicar no botão de compartilhar um meal a url é copiada', async () => {
+    const shareBtn = screen.getByTestId('0-horizontal-share-btn');
+    expect(shareBtn).toBeInTheDocument();
+
+    const clipboardText = 'http://localhost:3000/meals/52977';
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        readText: jest.fn().mockResolvedValue(clipboardText),
+        writeText: jest.fn().mockResolvedValue(),
+      },
+      writable: true,
+    });
+
+    userEvent.click(shareBtn);
+
+    await expect(navigator.clipboard.readText()).resolves.toBe(clipboardText);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(clipboardText);
+
+    const alert = screen.getByText('Link copied!');
+    expect(alert).toBeInTheDocument();
+  });
+  it('Testa se ao clicar no botão de compartilhar um drink a url é copiada', async () => {
+    const shareBtn = screen.getByTestId('1-horizontal-share-btn');
+    expect(shareBtn).toBeInTheDocument();
+
+    const clipboardText = 'http://localhost:3000/drinks/178319';
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        readText: jest.fn().mockResolvedValue(clipboardText),
+        writeText: jest.fn().mockResolvedValue(),
+      },
+      writable: true,
+    });
+
+    userEvent.click(shareBtn);
+
+    await expect(navigator.clipboard.readText()).resolves.toBe(clipboardText);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(clipboardText);
+
+    const alert = screen.getByText('Link copied!');
+    expect(alert).toBeInTheDocument();
+  });
+
+  it('Testa se ao clicar no botão meals apenas as receitas de comida são exibidas', () => {
+    const filterByFoodBtn = screen.getByTestId('filter-by-meal-btn');
+    expect(filterByFoodBtn).toBeInTheDocument();
+
+    userEvent.click(filterByFoodBtn);
+
+    const image = screen.getAllByTestId(/-horizontal-image/i);
+    const topText = screen.getAllByTestId(/-horizontal-top-text/i);
+    const name = screen.getAllByTestId(/-horizontal-name/i);
+    const doneDate = screen.getAllByTestId(/-horizontal-done-date/i);
+    const shareBtn = screen.getAllByTestId(/-horizontal-share-btn/i);
+
+    expect(image).toHaveLength(1);
+    expect(topText).toHaveLength(1);
+    expect(name).toHaveLength(1);
+    expect(doneDate).toHaveLength(1);
+    expect(shareBtn).toHaveLength(1);
+  });
+  it('Testa se ao clicar no botão drinks apenas as receitas de bebida são exibidas', () => {
+    const filterByDrinkBtn = screen.getByTestId('filter-by-drink-btn');
+    expect(filterByDrinkBtn).toBeInTheDocument();
+
+    userEvent.click(filterByDrinkBtn);
+
+    const image = screen.getAllByTestId(/-horizontal-image/i);
+    const topText = screen.getAllByTestId(/-horizontal-top-text/i);
+    const name = screen.getAllByTestId(/-horizontal-name/i);
+    const doneDate = screen.getAllByTestId(/-horizontal-done-date/i);
+    const shareBtn = screen.getAllByTestId(/-horizontal-share-btn/i);
+
+    expect(image).toHaveLength(1);
+    expect(topText).toHaveLength(1);
+    expect(name).toHaveLength(1);
+    expect(doneDate).toHaveLength(1);
+    expect(shareBtn).toHaveLength(1);
+
+    userEvent.click(screen.getByTestId('filter-by-all-btn'));
+
+    const image2 = screen.getAllByTestId(/-horizontal-image/i);
+    expect(image2).toHaveLength(2);
+  });
+  it('Testa se ao clicar no botão de favorite a receita é removidada da tela', () => {
+    const filterByFoodBtn = screen.getByTestId('filter-by-meal-btn');
+    expect(filterByFoodBtn).toBeInTheDocument();
+
+    userEvent.click(filterByFoodBtn);
+
+    const name = screen.getByTestId('0-horizontal-name');
+    const favoriteBtn = screen.getByTestId('0-horizontal-favorite-btn');
+
+    expect(name).toBeInTheDocument();
+    expect(name).toHaveTextContent('Spicy Arrabiata Penne');
+
+    userEvent.click(favoriteBtn);
+
+    const name1 = screen.getByTestId('0-horizontal-name');
+
+    expect(name1).toHaveTextContent('Aquamarine');
+  });
+});
