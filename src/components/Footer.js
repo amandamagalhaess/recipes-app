ccd5ab6c19de4182444f04af71ece36295eadfa2
Pedro Minutti <pedro.cminutti@gmail.com>
2023-07-18 15:33:37 -0300

    CSS Header & Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/icone-prato.png';
+import drinkIcon from '../images/icone-bebida.png';
 import '../styles/Footer.css';
 
 export default function Footer() {
   return (
     <footer data-testid="footer">
-      <a href="/meals">
-        <img src={ mealIcon } alt="Meal Icon" data-testid="meals-bottom-btn" />
-      </a>
       <a href="/drinks">
-        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
+        <img
+          className="drinkIcon"
+          src={ drinkIcon }
+          alt="Drink Icon"
+          data-testid="drinks-bottom-btn"
+        />
       </a>
+      <a href="/meals">
+        <img
+          className="mealIcon"
+          src={ mealIcon }
+          alt="Meal Icon"
+          data-testid="meals-bottom-btn"
+        />
+      </a>
+
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import profileIcon from '../images/profileIcon.svg';
-import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/icone-perfil.png';
+import searchIcon from '../images/icone pesquiar.png';
 import SearchBar from './SearchBar';
+import '../styles/Header.css';
+import iconeRecipes from '../images/ícone Recipes app.png';
+import logoRecipes from '../images/logo Recipes app.png';
+import plateIcon from '../images/icone-prato.png';
+import drinkIcon from '../images/icone-bebida.png';
+import favIcon from '../images/favIcon.png';
+import perfilYellow from '../images/perfilYellow.png';
+import doneRecipesIcon from '../images/doneRecipesIcon.png';
 
 export default function Header() {
   const location = useLocation();
@@ -43,28 +51,64 @@ export default function Header() {
     }
   };
 
+  const renderImage = () => {
+    if (getTitle() === 'Meals') {
+      return <img src={ plateIcon } alt="" style={ { width: '40px', height: '30px' } } />;
+    } if (getTitle() === 'Drinks') {
+      return <img src={ drinkIcon } alt="" style={ { width: '25px', height: '26px' } } />;
+    } if (getTitle() === 'Done Recipes') {
+      return (<img
+        src={ doneRecipesIcon }
+        alt=""
+        style={ { width: '40px', height: '39px' } }
+      />);
+    } if (getTitle() === 'Favorite Recipes') {
+      return <img src={ favIcon } alt="" style={ { width: '39px', height: '39px' } } />;
+    } if (getTitle() === 'Profile') {
+      return (<img
+        src={ perfilYellow }
+        alt=""
+        style={ { width: '30px', height: '30px' } }
+      />);
+    }
+    return null;
+  };
+
   return (
     <div>
-      <a
-        href="/profile"
-        onClick={ handleProfileClick }
-      >
-        <img
-          alt="Ícone de Perfil"
-          src={ profileIcon }
-          data-testid="profile-top-btn"
-        />
-      </a>
-      { showSearchIcon && (
-        <button onClick={ handleSearchClick }>
-          <img
-            alt="Ícone de Pesquisa"
-            src={ searchIcon }
-            data-testid="search-top-btn"
-          />
-        </button>
-      )}
-      <h1 data-testid="page-title">{getTitle()}</h1>
+      <header>
+        <div className="leftIcons">
+          <img className="recipeIcon" src={ iconeRecipes } alt="" />
+          <img className="logoIcon" src={ logoRecipes } alt="" />
+        </div>
+        <div className="rightIcons">
+          { showSearchIcon && (
+            <button className="searchIconBtn" onClick={ handleSearchClick }>
+              <img
+                alt="Ícone de Pesquisa"
+                src={ searchIcon }
+                data-testid="search-top-btn"
+                className="searchIcon"
+              />
+            </button>
+          )}
+          <a
+            href="/profile"
+            onClick={ handleProfileClick }
+          >
+            <img
+              alt="Ícone de Perfil"
+              src={ profileIcon }
+              data-testid="profile-top-btn"
+              className="profileIcon"
+            />
+          </a>
+        </div>
+      </header>
+      <div className="MealOrDrinkContainer">
+        {renderImage()}
+        <h1 data-testid="page-title">{getTitle()}</h1>
+      </div>
       <SearchBar />
     </div>
   );
